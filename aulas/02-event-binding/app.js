d5213de4f4b433c00cc8b32e9b0b13a5823391f6
Bia <95707803+biachalegre@users.noreply.github.com>
2024-08-09 10:33:15 -0300

    Início aula 02

---
+++ aulas/02-event-binding/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addCounter() {
+      this.counter++;
+    },
+    reduceCounter() {
+      this.counter--;
+    }
+  }
+});
+app.mount('#events');
