5c8a3935b348108ffac15d709ad0be95a6e4a0ef
biachalegre <bia.chalegre@outlook.com>
2024-08-12 13:57:48 -0300

    Watchers

--- aulas/02-event-binding/app.js
+++ aulas/02-event-binding/app.js
@@ -3,23 +3,53 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
-      confirmedName: '',
+      // confirmedName: '',
+      // fullname: '',
+      lastname: '',
     };
+  },
+  // Usar para -> HTTP Requests, timers 
+  watch: {
+    // Será executado sempre que a propriedade name for alterada
+    // Não retorna nada, apenas executa uma ação
+
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastname;
+    //   }
+    // },
+    // lastname(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+
+    // Resetar counter quando chega a 50
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
   },
   computed: {
     // computed properties são reativamente atualizadas, ou seja, sempre que uma propriedade que ela depende for atualizada, ela também será
     // não é usado como funções (não são chamadas), mas como propriedades
     fullName() {
-      if (this.name === '') {
+      if (this.name === '' || this.lastname === '') {
         return '';
       }
-      return this.name + ' ' + 'Silva';
+      return this.name + ' ' + this.lastname;
     }
   },
   // apenas usar métodos se quiser recalcular um valor quando qualquer coisa na página mudar (a função é chamada sempre que a página é renderizada)
   methods: {
     resetInput() {
       this.name = '';
+      this.lastname = '';
     },
     // confirmInput() {
     //   this.confirmedName = this.name;
