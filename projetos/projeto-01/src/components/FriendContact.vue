c87cda47d1c51291b239dc19a330872cefaacf39
biachalegre <bia.chalegre@outlook.com>
2024-08-13 14:04:06 -0300

    New Vue Project

---
+++ projetos/projeto-01/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAdress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['name', 'phoneNumber', 'emailAdress'],
+  data() {
+    return {
+      detailsAreVisible: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
