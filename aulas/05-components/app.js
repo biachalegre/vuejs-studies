035216e345334184a7377117fc97bab8e9d96935
biachalegre <bia.chalegre@outlook.com>
2024-08-13 05:38:52 -0300

    Introducing Componentes

--- aulas/05-components/app.js
+++ aulas/05-components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -18,10 +17,40 @@ const app = Vue.createApp({
       ]
     }
   },
+});
+
+// Um componente é um objeto que contém um template, data, methods, computed, etc.
+// O nome sempre deve ser em kebab-case.
+// Para conectar o componente com o HTML, usamos template
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{friend.name}}</h2>
+      <button @click="toggleDetails">
+        {{detailsAreVisible ? 'Hide' : 'Show'}} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{friend.phone}}</li>
+        <li><strong>Email:</strong>{{friend.email}}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '12345 67890',
+        email: 'manuel@localhost.com'
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     }
   },
-})
+});
+
 app.mount('#app')
